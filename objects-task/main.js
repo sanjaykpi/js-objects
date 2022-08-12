092762823e8e7671e94c9bc1aad5337c6986b9ff
Sanjay Selvamani <sanjayselvamani6@gmail.com>
2022-08-12 10:15:41 -0700

    js-obj

---
+++ objects-task/main.js
@@ -0,0 +1,149 @@
+// 1   car
+const car={Brand:"maruthi-suzuki",Name:"baleno",Color:"blue",FuelType:"diesel"};
+console.log("my car is", car.Name);
+console.log(car);
+
+// 2   bike
+const bike={Brand:"RoyalEnfield",Name:"classic 350",Color:"black",FuelType:"petrol"};
+console.log("my bike is",bike.Brand);
+console.log(bike);
+
+// 3  laptop
+const laptop={Brand:"Lenova",Model:"E41-25",Color:"black",Type:"Electric charge"};
+console.log("my laptop is",laptop.Brand);
+console.log(laptop);
+
+// 4  mobile
+const mobile={Brand:"poco",Model:"m2 pro",Color:"black",Camera:"48mp"};
+console.log("my mobile is",mobile.Brand);
+console.log(mobile);
+
+// 5  pencil
+const pencil={Brand:"Apsara",Color:"Green",Model:"2H",ues:"writing"};
+console.log("my pencil is",pencil.Brand);
+console.log(pencil);
+
+// 6  headphone
+const headphone={Brand:"Boat",Color:"black",Style:"wireless"};
+console.log("my headphone is",headphone.Brand);
+console.log(headphone);
+
+// 7  door
+const door={Type:"wood",Color:"brown",Brand:"sandel"};
+console.log("house door is",door.Brand);
+console.log(door);
+
+// 8  tv
+const tv={Brand:"Sony",Color:"black",Model:"LED",Size:"32"};
+console.log("my tv is",tv.Brand);
+console.log(tv);
+
+// 9  refri
+const refri={Brand:"LG",Color:"red",Use:"cooling system"};
+console.log("my refri is",refri.Brand);
+console.log(refri);
+
+// 10  player
+const player={Name:"Dhoni",Team:"India",No:"7",Style:"Finisher"};
+console.log("fav player",player.Name);
+console.log(player);
+
+// 11  watch
+const watch={Brand:"Sonata",Color:"golden",Use:"time"};
+console.log("my watch is",watch.Brand);
+console.log(watch);
+
+// 12  clock
+const clock={Brand:"Titan",Style:"latest",Color:"red"};
+console.log("my clock is",clock.Brand);
+console.log(clock);
+
+// 13  bio data
+const bio={Name:"Sanjay",study:"BA",bloodgroup:"o-"};
+console.log("my name",bio.Name);
+console.log(bio);
+
+// 14  helmet
+const helmet={Brand:"agv",Color:"light green",Size:"XL",Use:"production"};
+console.log("my helmet name",helmet.Brand);
+console.log(helmet);
+
+// 15  phone
+const phone={Brand:"oneplus",Name:"nord",Color:"blue",ram:"8"};
+console.log("my phone name",phone.Brand);
+console.log(phone);
+
+// 16  lion
+const lion={Brand:"king",Color:"golden brown",name:"lion"};
+console.log("the king of jungle in",lion.name);
+console.log(lion);
+
+// 17  cricket
+const cricket={Brand:"mr360",Name:"ABD",Country:"south africa",play:"cricket"};
+console.log("mr360 in",cricket.Name);
+console.log(cricket);
+
+// 18  social
+const insta={Brand:"app",Name:"insta",Use:"waste of time"};
+console.log("ues in insta of",insta.Use);
+console.log(insta);
+
+// 19  shirt
+const shirt={Brand:"otto",Name:"shirt",Color:"black",Model:"plane"};
+console.log("my fav shirt color in",shirt.Color);
+console.log(shirt);
+
+// 20  play
+const play={Game:"foodball",Name:"Ronaldo",No:"7"};
+console.log("foodball fav in",play.Name);
+console.log(play);
+
+// 21   school
+const study={Study:"school",Place:"PCM",Purpose:"knowledge"};
+console.log("studing purpose to increse tha",study.Purpose);
+console.log(study);
+
+// 22  bag
+const bag={Brand:"tycoon",Type:"bag",Color:"black"};
+console.log("many things store in the",bag.Type);
+console.log(bag);
+
+// 23   bucket
+const things={Name:"bucket",Type:"plastic",use:"save water"};
+console.log("bucket use for",things.use);
+console.log(things);
+
+// 24  washing mechine
+const thing={Brand:"LG",Name:"washing mechine",Color:"black",Use:"electric power"};
+console.log("washing easy for",thing.Name);
+console.log(thing);
+
+// 25  fan
+const fan={Brand:"usha",Name:"fan",Color:"black",Use:"cooling"};
+console.log("cooling tha room use for",fan.Name);
+console.log(fan);
+
+// 26  shoes
+const footer={Brand:"adidas",Type:"shoes",Color:"white",Use:"prodection"};
+console.log("footer prodection use for",footer.Type);
+console.log(footer);
+
+// 27   lights
+const power={Brand:"philips",Type:"light",Color:"white",Use:"brightness"};
+console.log("room brightness incress use to",power.Type);
+console.log(power);
+
+// 28  drimmer
+const drim={Brand:"philips",Name:"drimmer",Color:"red",Type:"charging type"};
+console.log("shape of beard use for",drim.Name);
+console.log(drim);
+
+// 29  charger
+const charger={Brand:"lenova",Name:"charger",Color:"black",Use:"charging"};
+console.log("charging use for",charger.Name);
+console.log(charger);
+
+// 30  window
+const room={Type:"wood",Color:"brown",Name:"window"};
+console.log("room inside use for",room.Name);
+console.log(room);
